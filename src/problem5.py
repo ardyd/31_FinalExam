69ab7a03a49e7445231f9905d16d91d04f5fb675
ardyd <ardyd@rose-hulman.edu>
2018-05-24 14:43:24 -0400

    done

--- src/problem5.py
+++ src/problem5.py
@@ -2,9 +2,9 @@
 Final exam, problem 5.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and David Ardy.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -140,6 +140,10 @@ def problem5(m, numbers1, numbers2):
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    for k in range(len(numbers1)):
+        if numbers1[k]*m == numbers2[k]:
+            return [numbers1[k], numbers2[k]]
+    return []
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
